4aedf6aedc60d8f83b2fac1639bb95e3ff6e8716
Jaxel Rojas <jaxelr@gmail.com>
2018-10-14 10:27:00 -0400

    Removed duplicated snippet.
    Added My Extensions sample.

---
+++ MyExtensions.cs
@@ -0,0 +1,13 @@
+void Main()
+{
+	// Write code to test your extensions here. Press F5 to compile and run.
+}
+
+public static class MyExtensions
+{
+	// Write custom extension methods here. They will be available to all queries.
+	public const string SQLConnectionString = "Server=.;database=master;Trusted_Connection=true;MultipleActiveResultSets=true;";
+	public const string DB2ConnectionString = "Database=HSCRP;Server=10.31.1.68:60016;UserID=appsuid;Password=Comp1ete4you;CurrentSchema=HS";
+}
+
+// You can also define non-static classes, enums, etc.
