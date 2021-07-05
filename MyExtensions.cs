a78d63606b17047762d70316746420afb723c04a
Jaxel Rojas <jaxelr@gmail.com>
2021-07-05 15:49:51 -0400

    chore: added local dbs connstrings (postgres/mysql)

--- MyExtensions.cs
+++ MyExtensions.cs
@@ -8,6 +8,8 @@ public static class MyExtensions
 	// Write custom extension methods here. They will be available to all queries.
 	public const string SQLConnectionString = "Server=.;database=master;Trusted_Connection=true;MultipleActiveResultSets=true;";
 	public const string DB2ConnectionString = "Database=HSCRP;Server=10.31.1.68:60016;UserID=appsuid;Password=Comp1ete4you;CurrentSchema=HS";
+	public static string PostgresConnectionString => "Host=localhost;Port=5432;database=postgres;User ID=postgres;Password=postgres";
+	public static string MySqlConnectionString => "Server=127.0.0.1;Port=3306;Database=test;User Id=root;";
 }
 
 // You can also define non-static classes, enums, etc.
